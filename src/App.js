198a5f14803813e7e2a0407337d09ae10e8f71aa
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-06-10 03:46:20 +0530

    added components(pages) folders and files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-const App = () => {
-  return <h1>Rendered</h1>;
-};
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import Plans from './pages/plans/Plans'
+import Trainers from './pages/trainers/Trainers'
+import NotFound from './pages/notFound/NotFound'
 
-export default App;
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+      <Gallery />
+      <Plans />
+      <Trainers />
+      <NotFound />
+    </div>
+  )
+}
+
+export default App
